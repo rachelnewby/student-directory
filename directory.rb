6a27a75a714bf06d058c668587b2ca2e050b2209
Rachel Newby <rachelcnewby@gmail.com>
2022-11-24 11:36:40 +0000

    Interactive menu refactored

--- directory.rb
+++ directory.rb
@@ -1,69 +1,69 @@
+@students = []
+
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
   #create an empty array
-  students = []
+  #students = [] - I don't think I need this
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "We now have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "We now have #{@students.count} students"
     #get another input from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end  
+end
+
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-=begin
-students = input_students
-print_header
-print(students)
-print_footer(students)
-=end
-
-
-
-def interactive_menu
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" 
-  # 2. read the input and save it into a variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
+
